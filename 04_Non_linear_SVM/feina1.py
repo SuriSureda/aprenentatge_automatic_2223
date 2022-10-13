ce389debe0a3391f6df13c30e9f9a806edef2ee5
Andreu Sureda <suredaps4@gmail.com>
2022-10-13 14:38:43 +0000

    Practica 4 : Feina 1

---
+++ 04_Non_linear_SVM/feina1.py
@@ -0,0 +1,47 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_classification
+from sklearn.svm import SVC
+
+# Generació del conjunt de mostres
+X, y = make_classification(n_samples=100, n_features=2, n_redundant=0, n_repeated=0,
+                           n_classes=2, n_clusters_per_class=2, class_sep=0.5,
+                           random_state=9)
+y[y == 0] = -1
+
+X_train, X_test, y_train, y_test = train_test_split(X, y,  shuffle=True, random_state=123, test_size=0.25)
+# Els dos algorismes es beneficien d'estandaritzar les dades
+scaler = StandardScaler()
+X_transformed = scaler.fit_transform(X_train)
+X_test_transformed = scaler.transform(X_test)
+
+## SVC defined linear kernel
+svm = SVC(kernel='linear')
+svm.fit(X_train, y_train);
+X_test_transformed = scaler.transform(X_test)
+
+## SVC with custom linear kernel
+def kernel_lineal(x1, x2):
+  return x1.dot(x2.T)
+	
+svmCustom = SVC(kernel=kernel_lineal)
+svmCustom.fit(X_train, y_train)
+
+svmCustom.predict(X_test) == svm.predict(X_test)
+
+# RESULTS
+y_predicted = svm.predict(X_test_transformed)
+y_customPredicted = svmCustom.predict(X_test_transformed)
+
+print("SVM Predict == SVM CUSTOM Predict : ", np.array_equal(y_predicted, y_customPredicted))
+
+# ERRORS BY SVM
+differences = (y_predicted - y_test)
+errors = np.count_nonzero(differences)
+print(f'[SVM] Rati d\'acerts en el bloc de predicció: {(len(y_predicted)-errors)/len(y_predicted)}')
+
+differences = (y_customPredicted - y_test)
+errors = np.count_nonzero(differences)
+print(f'[SVM CUSTOM]Rati d\'acerts en el bloc de predicció: {(len(y_predicted)-errors)/len(y_predicted)}')
