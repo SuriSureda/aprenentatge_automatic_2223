f5fac0048c7976d26502c228e716605cac361595
Andreu Sureda <suredaps4@gmail.com>
2022-10-28 21:17:29 +0000

    TALLER 5

--- 05_Generative_Models/main.py
+++ 05_Generative_Models/main.py
@@ -1,4 +1,7 @@
+# Andreu Sureda
+
 import matplotlib.pyplot as plt
+from pyparsing import col
 from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
 import numpy as np
@@ -15,7 +18,66 @@ def plot_digits(data):
 
 
 digits = datasets.load_digits()
-plot_digits(digits.data[:100, :])
 
+X = digits.data
+
+pca = PCA()
+pca_components = pca.fit_transform(X)
+pca_variance = pca.explained_variance_ratio_
+
+acc_variance = np.cumsum(pca_variance)
+plt.plot(acc_variance)
+
+# SELECCIÓ MILLOR NOMBRE DE COMPONENTS PCA
+def get_best_n_components(variances, min = 0.96, diff = 0.005):
+    n_components= 1
+    acc_var = variances[0]
+    prev_acc = 0
+    while (acc_var < min or acc_var - prev_acc > diff): 
+        prev_acc = acc_var
+        acc_var = variances[n_components]
+        n_components +=1
+    return (n_components, acc_var)
+
+best_n, variance = get_best_n_components(acc_variance)
+plt.axvline(x = best_n, color='r')
+plt.axhline(y = 0.96, color = 'b', linestyle='--', label='Minimum variance')
+plt.axhline(y = variance, color = 'g', label='Actual variance')
+plt.legend()
+# plt.savefig("accumulate_variance.png")
+# plt.clf()
 plt.show()
-print(digits.data.shape)
+
+# PCA AMB MILLOR N
+pca = PCA(n_components=best_n)
+pca_components = pca.fit_transform(X)
+
+# MIXTURA DE GAUSSIANES
+def get_gaussian_bic_list(values, max_rang):
+    _list = []
+    for i in range(1, max_rang+1):
+        gm = GaussianMixture(n_components=i, random_state=1)
+        gm.fit(values)
+        bic = gm.bic(values)
+        _list.append(bic)
+    return _list
+
+
+gm_bic_list = get_gaussian_bic_list(pca_components, 10)
+best_gm_n_components = np.argmin(gm_bic_list) + 1
+plt.axvline(x = best_gm_n_components, color='r')
+plt.plot(list(range(1,11)), gm_bic_list)
+# plt.savefig("gaussian_bic.png")
+# plt.clf()
+plt.show()
+
+# GENERACIÓ
+gm = GaussianMixture(n_components= best_gm_n_components, random_state=1)
+gm.fit(pca_components)
+
+samples = gm.sample(100)
+generated_samples = pca.inverse_transform(samples[0])
+plot_digits(generated_samples[:100, :])
+plt.show()
+# plt.savefig("generated_numbers.png")
+# plt.clf()
